e53f79b2d4458975e110d201a0d19e99845402ab
kramer001mwangala <115226116+kramer001mwangala@users.noreply.github.com>
2023-06-12 15:51:27 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ document.querySelector('.read-more').addEventListener('click', function(e) {
     contentDiv.classList.toggle('show-more');
     this.textContent = contentDiv.classList.contains('show-more') ? 'Read Less' : 'Read More';
   });
- 
+ // server
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbx5bCvxgSBNbk6Jad_Yusgv4kPvFboi5OmemMesok-yO9SWFoX3_o3fRlHfhk7qBeeg/exec'
+ const form = document.forms['submit-to-google-sheet']
 
-  
-  
+ form.addEventListener('submit', e => {
+   e.preventDefault()
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+     .then(response => console.log('Success!', response))
+     .catch(error => console.error('Error!', error.message))
+ })
+ /*function submit () {
+    alert("Thank you, your response has been received.");
+ }*/
+ /*function submit() {
+    console.log("Thank you, your response has been received.");
+  }*/
+  function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you, your response has been received.");
+    // Additional code to handle form submission
+  }
